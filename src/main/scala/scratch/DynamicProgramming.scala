b0d7abd5f45ecbc5de79b34fc241d36ab54a5962
Aaron Silinskas <asilinskas@BELC02RN8R6G8WN.sea.corp.expecn.com>
2016-10-30 15:33:47 -0700

    Removed recursion from minimum steps DP

--- src/main/scala/scratch/DynamicProgramming.scala
+++ src/main/scala/scratch/DynamicProgramming.scala
@@ -4,25 +4,19 @@ object DynamicProgramming {
 
   def minimumStepsTo1(value: Int): Int = {
     val memo: Array[Int] = Array.ofDim(value + 1)
-
-    def iter(value: Int): Int = {
-      if (value == 1) {
-        0
-      } else {
-        if (memo(value) == 0) {
-          memo(value) = 1 + iter(value - 1)
-          if (value % 2 == 0) {
-            memo(value) = Math.min(memo(value), 1 + iter(value / 2))
-          }
-          if (value % 3 == 0) {
-            memo(value) = Math.min(memo(value), 1 + iter(value / 3))
-          }
-        }
-        memo(value)
+    memo(0) = 0
+    memo(1) = 0
+    for (n <- 2 to value) {
+      memo(n) = 1 + memo(n - 1)
+      if (n % 2 == 0) {
+        memo(n) = Math.min(memo(n), 1 + memo(n / 2))
+      }
+      if (n % 3 == 0) {
+        memo(n) = Math.min(memo(n), 1 + memo(n / 3))
       }
     }
 
-    iter(value)
+    memo(value)
   }
-  
+
 }
