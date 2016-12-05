95552f4cfbb8d0338f168c29b1361838afdaee2a
Aaron Silinskas <asilinskas@BELC02RN8R6G8WN.sea.corp.expecn.com>
2016-12-04 18:29:58 -0800

    WIP: snapshotting because I need to switch to other work

--- src/main/scala/scratch/elevator/Scheduler.scala
+++ src/main/scala/scratch/elevator/Scheduler.scala
@@ -9,6 +9,10 @@ class Scheduler(elevator: Elevator) {
   def run() = {
     val elevatorFloor = elevator.floor()
 
+    val (pickups, dropOffs) = scheduled.partition(_.isInstanceOf[PickupRequest])
+
+    val direction = elevatorDirection(elevatorFloor, dropOffs)
+
     val orderedScheduled = scheduled.sortBy {
       case pr: PickupRequest => Math.abs(pr.floor - elevatorFloor)
       case dr: DropOffRequest => Math.abs(dr.floor - elevatorFloor)
@@ -20,18 +24,26 @@ class Scheduler(elevator: Elevator) {
     }
   }
 
-  def requestPickup(floor: Int, direction:ElevatorDirection): Unit = {
+  def requestPickup(floor: Int, direction: ElevatorDirection): Unit = {
     scheduled += PickupRequest(floor, direction)
   }
 
   def requestDropOff(floor: Int): Unit = {
     scheduled += DropOffRequest(floor)
   }
+
+  private def elevatorDirection(elevatorFloor: Int, destinationFloors: mutable.Queue[SchedulerRequest]):ElevatorDirection = {
+    val nextFloor = destinationFloors.front.floor()
+    if (elevatorFloor < nextFloor) Up
+    else Down
+  }
 }
 
 
-sealed trait SchedulerRequest
+sealed trait SchedulerRequest {
+  def floor(): Int
+}
 
-case class PickupRequest(floor: Int, direction:ElevatorDirection) extends SchedulerRequest
+case class PickupRequest(floor: Int, direction: ElevatorDirection) extends SchedulerRequest
 
 case class DropOffRequest(floor: Int) extends SchedulerRequest
